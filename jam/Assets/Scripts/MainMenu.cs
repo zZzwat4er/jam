beebb32ff62821a639ccdf740fec48f4d21b8e15
maksktl <matantsev0@gmail.com>
2020-06-14 03:15:56 +0600

    Сделал начальное меню

---
+++ jam/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject main_menu;
+
+    public void on_click()
+    {
+        main_menu.active = false;
+    }
+}
