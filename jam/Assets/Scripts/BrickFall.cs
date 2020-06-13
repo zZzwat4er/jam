a85fb7e73e3e42bee2e1330c44e1bc150cdf7a15
zZzwat4er <valenten2001@mail.ru>
2020-06-14 00:16:42 +0300

    Score is added

--- jam/Assets/Scripts/BrickFall.cs
+++ jam/Assets/Scripts/BrickFall.cs
@@ -6,20 +6,64 @@ using UnityEngine;
 
 public class BrickFall : MonoBehaviour
 {
+    private static int currentLevel = 1;
+    private static int collisionCount = 0;
+    private bool isCollided;
     private GameObject blockCreator;
-
+    private GameObject ground;
+    private BrickInfo info;
+    
     public float speed = 10f;
+
+    private void Awake()
+    {
+        ground = GameObject.FindWithTag("Ground");
+    }
 
     private void OnEnable()
     {
+        isCollided = false;
+        info = gameObject.GetComponent<BrickInfo>();
+        info.lvl = currentLevel;
         blockCreator = GameObject.FindWithTag("blockCreator");
     }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        blockCreator.GetComponent<CreateBlock>().enabled = true;
-        blockCreator.GetComponent<CreateBlock>().enabled = false;
-        Destroy(gameObject.GetComponent<BrickFall>());
+        Debug.Log("col");
+        if(!isCollided)
+        {
+            // score calculation
+            if (col.gameObject == ground && currentLevel == 1)
+            {
+                collisionCount++;
+                Score.score += collisionCount;
+            }
+            else
+            {
+                if (col.gameObject.GetComponent<BrickInfo>().lvl == currentLevel - 1)
+                {
+                    collisionCount++;
+                    Score.score += collisionCount * currentLevel;
+                }
+                else if (col.gameObject.GetComponent<BrickInfo>().lvl == currentLevel)
+                {
+                    currentLevel++;
+                    collisionCount = 0;
+                    Score.score += currentLevel;
+                }
+                else
+                {
+                    //todo get damage
+                }
+            }
+
+            info.lvl = currentLevel;
+            isCollided = true;
+            blockCreator.GetComponent<CreateBlock>().enabled = true;
+            blockCreator.GetComponent<CreateBlock>().enabled = false;
+            Destroy(gameObject.GetComponent<BrickFall>());
+        }
     }
 
     private void FixedUpdate()
---
+++ jam/Assets/Scripts/Score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static int score;
+    public Text text;
+
+
+    private void Start()
+    {
+        score = 0;
+    }
+
+    private void Update()
+    {
+        text.text = "Score: " + score;
+    }
+}
