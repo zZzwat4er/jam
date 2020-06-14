477254552d8fa3ed95cc8002bc900cbb6c248ab3
zZzwat4er <valenten2001@mail.ru>
2020-06-14 14:52:25 +0300

    HP BAR

--- jam/Assets/Scripts/Score.cs
+++ jam/Assets/Scripts/Score.cs
@@ -5,19 +5,50 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
+
     public static int score; // current score
     public static int lives = 5;
     public Text text; // text where we put score
-    public Text Lives;
+    public GameObject Lives;
 
+    [SerializeField] private Sprite HP_5;
+    [SerializeField] private Sprite HP_4;
+    [SerializeField] private Sprite HP_3;
+    [SerializeField] private Sprite HP_2;
+    [SerializeField] private Sprite HP_1;
+    [SerializeField] private Sprite HP_0;
+
+    private Image currentImage;
+    
     private void Start()
     {
         score = 0;
+        currentImage = Lives.GetComponent<Image>();
     }
 
     private void Update()
     {
         text.text = "Score: " + score; // print score
-        Lives.text = lives + "";
+        switch (lives)
+        {
+            case 5:
+                currentImage.sprite = HP_5;
+                break;
+            case 4:
+                currentImage.sprite = HP_4;
+                break;
+            case 3:
+                currentImage.sprite = HP_3;
+                break;
+            case 2:
+                currentImage.sprite = HP_2;
+                break;
+            case 1:
+                currentImage.sprite = HP_1;
+                break;
+            default:
+                currentImage.sprite = HP_0;
+                break;
+        }
     }
 }
